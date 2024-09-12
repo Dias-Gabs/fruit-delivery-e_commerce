6554995dfd247d604bce03a168864b62b8d75af2
Dias-Gabs <diasg.contato@gmail.com>
2024-09-11 21:05:39 -0300

    Menu list, Food & List Display, Footer error - Running, not clear

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,14 +8,17 @@ import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 
 const App = () => {
   return (
-    <div className='app'>
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/cart' element={<Cart/>}/>
-        <Route path='/order' element={<PlaceOrder/>}/>
-      </Routes>
-    </div>
+    <>
+      <div className='app'>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/cart' element={<Cart/>}/>
+          <Route path='/order' element={<PlaceOrder/>}/>
+        </Routes>
+      </div>
+      {/* <Footer /> */}
+    </>
   )
 }
 
