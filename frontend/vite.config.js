63b98c7e8047edefc0770b9919d83ad6a86a02b9
Dias <143370530+Dias-Gabs@users.noreply.github.com>
2024-09-15 20:00:24 -0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 9080,  // Define a porta para 9080
-  },
+    host: true, // Habilita acesso via rede
+    port: 9080 // Ou qualquer porta que preferir
+  }
 })
