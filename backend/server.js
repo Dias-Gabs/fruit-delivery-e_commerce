a8c30e4bd320a58b734c2a122d62ac2be020bf10
Dias <diasg.contato@gmail.com>
2024-09-17 20:59:30 -0300

    FrontEnd modification, FoodDisplay - Working clear

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import express from "express"
+import cors from "cors"
+import { connectDB } from "./config/db.js"
+import foodRouter from "./routes/foodRoute.js"
+
+// - App Config
+const app = express()
+const port = 4000
+
+// - Middleware
+app.use(express.json())
+app.use(cors())
+
+// Db Connetion:
+connectDB();
+
+// Api Endpoints
+app.use("/api/food",foodRouter)
+
+app.get("/",(req,res)=>{
+    res.send("- #LOG: API Working")
+})
+
+app.listen(port,()=>{
+    console.log(`- #LOG: Server started on http://localhost:${port}`) 
+})
+
+// mongodb+srv://fruitfresh:198198Gg$@cluster0.82xg4.mongodb.net/?
