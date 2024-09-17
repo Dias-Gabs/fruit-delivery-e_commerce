146835295335ecea775f89b816706b11abde033a
Dias <diasg.contato@gmail.com>
2024-09-17 20:58:31 -0300

    FrontEnd modification - FoodDisplay

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import './FoodDisplay.css'
+import './FoodItem.css'
 import { assets } from "../../assets/assets";
 import { StoreContext } from "../../context/StoreContext";
 
@@ -13,15 +13,19 @@ const FoodItem = ({id,name,price,description,image}) => {
 
             <div className="food-item-img-container">
                 <img className="food-item-image" src={image} alt=""/>
-                {!cartItems[id]
-                  ?<img className="add" onClick={()=>addToCart(id)} src={assets.add_icon_white} />  
-                  :<div className="food-item-counter">
-                        <img onClick={()=>removeFromCart(id)} src={assets.remove_icon_red} alt=""/>
-                        <p>{cartItems[id]}</p>
-                        <img onClick={()=>addToCart(id)} src={assets.add_icon_green} alt=""/>
-                    </div>
-                }
             </div>
+
+        <div className="container-buttons-item">
+            {!cartItems[id]
+                    ?<img className="add" onClick={()=>addToCart(id)} src={assets.add_icon_white} />  
+                    :<div className="food-item-counter">
+                            <img onClick={()=>removeFromCart(id)} src={assets.remove_icon_red} alt=""/>
+                            <p>{cartItems[id]}</p>
+                            <img onClick={()=>addToCart(id)} src={assets.add_icon_green} alt=""/>
+                        </div>
+                    }
+        </div>
+
             <div className="food-item-info">
                 <div className="food-item-name-rating">
                     <p>{name}</p>
