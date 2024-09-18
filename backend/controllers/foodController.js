9a970dcc4dc329868b28960eb3a1614b1726fd42
Dias <diasg.contato@gmail.com>
2024-09-18 14:56:09 -0300

    Getting everything up to date, no additions have been commited/pushed. - Working clear.

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -16,12 +16,37 @@ const addFood = async (req,res) => {
 
     try {
         await food.save();
-        res.json({sucess:true,message:"Food Added"})
+        res.json({sucess:true,message:"- #LOG: Food Added"})
     } catch (error){
         console.log(error)
-        res.json({sucess:false,message:"Error"})
+        res.json({sucess:false,message:"- #LOG-ERROR: Error in the const 'addFood', try-catch method. (File: '../backend/controllers/foodController.js'."})
     }
 
 }
 
-export {addFood}
+// All food list
+const listFood = async(req,res) => {
+    try {
+        const foods = await foodModel.find({});
+        res.json({sucess:true,data:foods})
+    } catch (error){
+        console.log(error);
+        res.json({sucess:false,message:"- #LOG-ERROR: Error in the const 'listFood', try-catch method. (File: '../backend/controllers/foodController.js'."})
+    }
+}
+
+// Remove food item
+const removeFood = async(req,res) => {
+    try{
+        const food = await foodModel.findById(req.body.id);
+        fs.unlink(`uploads/${food.image}`,()=>{})
+
+        await foodModel.findByIdAndDelete(req.body.id);
+        res.json({sucess:true,message:"- #LOG: Food removed!"})
+    } catch (error){
+        console.log(error);
+        res.json({sucess:false,message:"- #LOG-ERROR: Error in the const 'removeFood' in the file '../backend/controllers/foodController.js'."})
+    }
+}
+
+export {addFood,listFood,removeFood}
