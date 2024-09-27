d3ef459b0c24f692f43db403b5f28ba94a95a026
Dias <143370530+Dias-Gabs@users.noreply.github.com>
2024-09-27 09:40:17 -0300

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://fruitfresh:198198Gg$@cluster0.82xg4.mongodb.net/fruitfresh-ecommerce').then(()=>console.log("- #LOG: MongoDB Database connected!"));
+    await mongoose.connect('API-KEY').then(()=>console.log("- #LOG: MongoDB Database connected!"));
 }
